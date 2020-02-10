32896f83ebae48c2b73cd2a581b6fa06761c5ab7
Saurabh <saurabhkumar@teknuance.com>
2020-02-10 18:32:06 +0530

    accordion changes

--- src/common/Acordion/index.js
+++ src/common/Acordion/index.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Accordion, Card } from "react-bootstrap";
 import "./Acordion.css";
 
 function Acordion(props) {
+  const [chevron, setChevron] = useState("fa fa-chevron-right");
+  const accordionClickFun = () => {
+    if (chevron === "fa fa-chevron-right") {
+      setChevron("fa fa-chevron-down");
+    } else if (chevron === "fa fa-chevron-down") {
+      setChevron("fa fa-chevron-right");
+    }
+  };
   return (
     <React.Fragment>
       <Accordion className="accordion">
-        <Card>
+        <Card onClick={accordionClickFun}>
           <Accordion.Toggle as={Card.Header} eventKey="0">
-            Click me!
+            <span>Click me!</span>
+            <i class={chevron} aria-hidden="true"></i>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey="0">
             <Card.Body>Hello! I'm the body</Card.Body>
