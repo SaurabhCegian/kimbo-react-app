bd9143534c8d1a2dd9ab3ed1e0ab5b2da97eabab
Saurabh Sharma <saurabh.cegian@gmail.com>
2020-02-22 22:53:04 +0530

    deleted unwanted code

--- src/components/FormProgression/constants.js
+++
@@ -1,6 +0,0 @@
-export const companySourceFund = [
-  "Savings from employment",
-  "Profits generated from business",
-  "Investment gain",
-  "Loans"
-];
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from '../../assets/images/logo.png';
-import profile from '../../assets/images/profile.png';
+import logo from "../../assets/images/logo.png";
+import profile from "../../assets/images/profile.png";
 
 function Header() {
   return (
